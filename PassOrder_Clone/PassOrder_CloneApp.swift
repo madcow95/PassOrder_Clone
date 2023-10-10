91a04f4cb17cfa0b2991c1c187b86db27c675759
madcow95 <lobasketve@naver.com>
2023-10-10 21:53:51 +0900

    Update README.md
    Update README.md File

---
+++ PassOrder_Clone/PassOrder_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  PassOrder_CloneApp.swift
+//  PassOrder_Clone
+//
+//  Created by ChoiKwangWoo on 2023/10/10.
+//
+
+import SwiftUI
+
+@main
+struct PassOrder_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # PassOrder_Clone
+
+## 패스오더 앱 홈화면 클론코딩
